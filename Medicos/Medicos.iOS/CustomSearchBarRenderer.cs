8475a957a037faac5e00526c575bb30274c3d18a
alonsodev <alonso.palacios.c@gmail.com>
2018-07-12 15:06:52 -0500

    b

---
+++ Medicos/Medicos.iOS/CustomSearchBarRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using Foundation;
+using Medicos.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using Medicos.CustomSearchBar;
+
+[assembly: ExportRenderer(typeof(CustomSearchBar), typeof(CustomSearchBarRenderer))]
+namespace Medicos.iOS
+{
+    public class CustomSearchBarRenderer : SearchBarRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                var view = (Medicos.CustomSearchBar.CustomSearchBar)Element;
+
+                /*Control.LeftView = new UIView(new CGRect(0f, 0f, 9f, 20f));
+                Control.LeftViewMode = UITextFieldViewMode.Always;*/
+                Control.KeyboardAppearance = UIKeyboardAppearance.Dark;
+                Control.ReturnKeyType = UIReturnKeyType.Done;
+                Control.Layer.CornerRadius = Convert.ToSingle(view.CornerRadius);
+                Control.Layer.BorderColor = view.BorderColor.ToCGColor();
+                Control.Layer.BorderWidth = view.BorderWidth;
+                Control.ClipsToBounds = true;
+            }
+        }
+    }
+}
