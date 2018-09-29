89c2b83e3b16ebd688b13c3e457119a5332d3266
alonsodev <alonso.palacios.c@gmail.com>
2018-09-29 16:15:45 -0500

    alonso

---
+++ Medicos/Medicos.iOS/NullableDatePickerRenderer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Medicos.CustomDatePicker;
+using Medicos.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ExportRenderer(typeof(CustomDatePicker), typeof(NullableDatePickerRenderer))]
+namespace Medicos.iOS
+{
+    public  class NullableDatePickerRenderer : DatePickerRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && this.Control != null)
+            {
+                this.AddClearButton();
+
+                this.Control.BorderStyle = UITextBorderStyle.Line;
+                Control.Layer.BorderColor = UIColor.LightGray.CGColor;
+                Control.Layer.BorderWidth = 1;
+
+                if (Device.Idiom == TargetIdiom.Tablet)
+                {
+                    this.Control.Font = UIFont.SystemFontOfSize(25);
+                }
+            }
+
+        }
+
+        private void AddClearButton()
+        {
+            var originalToolbar = this.Control.InputAccessoryView as UIToolbar;
+
+            if (originalToolbar != null && originalToolbar.Items.Length <= 2)
+            {
+                var clearButton = new UIBarButtonItem("Limpiar", UIBarButtonItemStyle.Plain, ((sender, ev) =>
+                {
+                    Medicos.CustomDatePicker.CustomDatePicker baseDatePicker = this.Element as Medicos.CustomDatePicker.CustomDatePicker;
+                    this.Element.Unfocus();
+                    this.Element.Date = DateTime.Now;
+                    baseDatePicker.CleanDate();
+
+                }));
+
+                var newItems = new List<UIBarButtonItem>();
+                foreach (var item in originalToolbar.Items)
+                {
+                    newItems.Add(item);
+                }
+
+                newItems.Insert(0, clearButton);
+
+                originalToolbar.Items = newItems.ToArray();
+                originalToolbar.SetNeedsDisplay();
+            }
+
+        }
+        
+
+    }
+}
